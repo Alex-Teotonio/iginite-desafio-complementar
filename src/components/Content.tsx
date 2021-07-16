767ce93cde5cc461d46470dcf42798dced3226be
Alex Barbosa Teotonio <alexteotonio29@gmail.com>
2021-07-15 23:57:38 -0300

    Finalizando Desafio complementar - trilha ReactJs

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,68 @@
-export function Content() {
-  // Complete aqui
+import { useEffect, useState } from 'react';
+import { api } from '../services/api';
+
+import '../styles/content.scss';
+import '../styles/global.scss';
+
+import { MovieCard } from '../components/MovieCard';
+
+interface ContentProps {
+  genreid:number
+}
+interface GenreResponseProps {
+  id: number;
+  name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+  title: string;
+}
+
+interface MovieProps {
+  imdbID: string;
+  Title: string;
+  Poster: string;
+  Ratings: Array<{
+    Source: string;
+    Value: string;
+  }>;
+  Runtime: string;
+}
+
+export function Content(props:ContentProps) {
+  const [genres, setGenres] = useState<GenreResponseProps[]>([]);
+
+  const [movies, setMovies] = useState<MovieProps[]>([]);
+  const [selectedGenre, setSelectedGenre] = useState<GenreResponseProps>({} as GenreResponseProps);
+
+  useEffect(() => {
+    api.get<GenreResponseProps[]>('genres').then(response => {
+      setGenres(response.data);
+    });
+  }, []);
+
+  useEffect(() => {
+    api.get<MovieProps[]>(`movies/?Genre_id=${props.genreid}`).then(response => {
+      setMovies(response.data);
+    });
+
+    api.get<GenreResponseProps>(`genres/${props.genreid}`).then(response => {
+      setSelectedGenre(response.data);
+    })
+  }, [props.genreid]);
+
+  return (
+    <>
+      <div className="container">
+        <header>
+          <span className="category">Categoria:<span> {selectedGenre.title}</span></span>
+        </header>
+
+        <main>
+          <div className="movies-list">
+            {movies.map(movie => (
+              <MovieCard key ={movie.imdbID} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
+            ))}
+          </div>
+        </main>
+      </div>
+    </>
+  )
 }
